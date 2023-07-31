7ce1d2c15e50b9afc9922f63da3e2999523a3e98
Vpn26 <140488611+Vpn26@users.noreply.github.com>
2023-07-31 16:34:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import "./styles.css";
+import { useState } from "react";
+export default function App3() {
+  const [option, setOption] = useState();
+  //  value is the current amount we are deopsit or withdraw
+  const [value, setValue] = useState();
+  // Amount is the account balance
+  const [amount, setAmount] = useState(0);
+  function main(e) {
+    // e.preventDefault which prevents the event that is ocuur.
+    e.preventDefault();
+    if (!option) alert("Please select Deposit or Withdraw");
+    else if (option === "Deposit") {
+      setAmount(Number(value) + amount);
+      console.log(amount);
+    } else {
+      const temp = amount - Number(value);
+      if (temp < 0) {
+        alert("Insufficient Balance");
+      } else {
+        setAmount(temp);
+      }
+    }
+    setValue("");
+  }
+
+  return (
+    <div className="First">
+      <center>
+        <h1 className="head">AUTOMATED TELLER MACHINE </h1>
+      </center>
+      <form onSubmit={main}>
+        <br></br>
+        <div>
+          <label className="label">
+            Please Choose Your Transaction
+            <br></br>
+            <br />
+            <select onChange={(e) => setOption(e.target.value)}>
+              <option />
+              <option> Deposit </option>
+              <option> Withdraw </option>
+            </select>
+            <br></br>
+            <br></br>
+            <center>
+              {" "}
+              <h2> Your Balance is Rs.{amount}</h2>
+            </center>
+            Enter the amount
+          </label>
+        </div>
+        <br></br>
+        <input
+          type="Number"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <div>
+          <br></br>
+          <button className="ok"> SUBMIT </button>
+        </div>
+      </form>
+    </div>
+  );
+}
